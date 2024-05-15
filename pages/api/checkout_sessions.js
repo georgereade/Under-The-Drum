78f2a34cc3e4b6458162ec99855da0816ffe17cf
GeorgeReade <georgereade@hotmail.co.uk>
2024-05-15 14:56:04 +0100

    Added success and cancel pages

--- pages/api/checkout_sessions.js
+++ pages/api/checkout_sessions.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
         mode: "payment",
         // success_url: `${req.headers.origin}/?success=true`,
         success_url: `${req.headers.origin}/success`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
+        cancel_url: `${req.headers.origin}/cancel`,
         automatic_tax: { enabled: true },
       });
       res.redirect(303, session.url);
--- pages/cancel.js
+++ pages/cancel.js
@@ -1,13 +1,13 @@
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Success() {
+export default function Cancel() {
     return (
       <main
         className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
       >
         <div className="z-10 max-w-5xl w-full items-center font-mono text-sm lg:flex">
-          <p> Payment successful, thanks! Your ticket will be emailed to you shortly.</p>
+          <p> Payment cancelled.</p>
         </div>
       </main>
     );
