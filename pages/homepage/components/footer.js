4023ea910ca99109525bca64df8eaca7bb6aeb5e
georgereade <georgereade@hotmail.co.uk>
2024-06-18 17:27:38 +0100

    fixed footer

--- pages/homepage/components/footer.js
+++ pages/homepage/components/footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Footer() {
   return (
@@ -16,14 +17,14 @@ export default function Footer() {
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <a href="#" className="hover:underline me-4 md:me-6">
+            <Link href="#" className="hover:underline me-4 md:me-6">
               Privacy Policy
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="/#contact" className="hover:underline">
+            <Link href="/#contact" className="hover:underline">
               Contact
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
