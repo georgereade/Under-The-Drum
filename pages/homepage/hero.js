e95eeb6241c6fcf8d4c48687585e8db612b908fc
georgereade <georgereade@hotmail.co.uk>
2024-06-23 21:23:19 +0100

    parallax

--- pages/homepage/hero.js
+++ pages/homepage/hero.js
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import UtdBanner from "./components/utdBanner";
 import BuyTickets from "./components/buyTickets";
+// import { Parallax } from "react-scroll-parallax";
 
 export default function Hero() {
   return (
@@ -9,18 +10,23 @@ export default function Hero() {
       className="flex flex-col items-center px-0 mx-0 pb-0 my-0 bg-utd-navy relative"
       id="hero"
     >
-      <div className="main-logo place-content-center w-screen h-4/5 sm:h-1/2 md:h-4/5 ">
+      {" "}
+      {/* <Parallax speed={-20}> */}
+      <div className="main-logo place-content-center w-screen h-4/5 sm:h-1/2 md:h-4/5 -z-50">
         <Image
           src="/169poster3.jpg"
-          className="hidden xl:block w-screen -my-6"
+          className="hidden xl:block w-screen h-full -my-6"
           alt="Under The Drum"
           width="2048"
           height="950"
           priority
         />
+      </div>
+      {/* </Parallax> */}
+      <div className="main-logo place-content-center w-screen h-4/5 sm:h-1/2 md:h-4/5">
         <Image
           src="/169poster.jpg"
-          className="hidden sm:block xl:hidden -mt-6 object-contain"
+          className="hidden sm:block xl:hidden h-full -mt-6"
           alt="Under The Drum"
           width="2048"
           height="950"
@@ -28,14 +34,14 @@ export default function Hero() {
         />
         <Image
           src="/PosterA3.jpg"
-          className="w-screen sm:hidden lg:hidden"
+          className="block w-screen sm:hidden lg:hidden"
           alt="Under The Drum"
           width="500"
           height="500"
           priority
         />
       </div>
-      <div className="md:w-full flex flex-col items-center bg-utd-navy md:px-12 h-full">
+      <div className="md:w-screen flex flex-col items-center bg-utd-navy md:px-12 h-full z-10">
         {/* <h1
           className={`font-light text-4xl md:text-5xl pt-4 leading-loose text-center uppercase ${roboto.className}`}
         >
@@ -49,7 +55,6 @@ export default function Hero() {
         </p>
         <BuyTickets />
       </div>
-
       <UtdBanner />
     </div>
   );
