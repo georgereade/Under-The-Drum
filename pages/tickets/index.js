9ccae23a2a0a6276b71319c838f4c79a5fe35eb3
georgereade <georgereade@hotmail.co.uk>
2025-01-27 15:42:48 +0000

    temporarily remove other page listings

--- pages/tickets/index.js
+++ pages/tickets/index.js
@@ -16,40 +16,40 @@ const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
 );
 
-export default function Tickets() {
-  return (
-    <div
-      className={`flex flex-col overflow-x-hidden dark items-center bg-utd-navy w-screen pb-12 min-h-full ${inconsolata.className} text-center bg-[url('/Drawings.png')] bg-cover bg-top bg-repeat-y bg-local`}
-      id="tickets"
-    >
-      <Head>
-        <title>Tickets | Under The Drum</title>
-        <meta
-          property="og:title"
-          content="Tickets | Under The Drum"
-          key="title"
-        />
-        <meta
-          name="description"
-          content="Tickets on sale now: £50 day tickets, £65 camping | A new music festival in the Antrim hills, featuring Joshua Burnside | 28th September 2024"
-          key="desc"
-        />
-      </Head>
-      <h1 className="font-extrabold text-2xl py-6 text-center leading-loose uppercase text-white">
-        Under The Drum Tickets
-      </h1>
-      <p>Tickets are no longer on sale.</p>
-      <p>Keep an eye out for information about next year...</p>
-      <TicketPrices />
+// export default function Tickets() {
+//   return (
+//     <div
+//       className={`flex flex-col overflow-x-hidden dark items-center bg-utd-navy w-screen pb-12 min-h-full ${inconsolata.className} text-center bg-[url('/Drawings.png')] bg-cover bg-top bg-repeat-y bg-local`}
+//       id="tickets"
+//     >
+//       <Head>
+//         <title>Tickets | Under The Drum</title>
+//         <meta
+//           property="og:title"
+//           content="Tickets | Under The Drum"
+//           key="title"
+//         />
+//         <meta
+//           name="description"
+//           content="Tickets on sale now: £50 day tickets, £65 camping | A new music festival in the Antrim hills, featuring Joshua Burnside | 28th September 2024"
+//           key="desc"
+//         />
+//       </Head>
+//       <h1 className="font-extrabold text-2xl py-6 text-center leading-loose uppercase text-white">
+//         Under The Drum Tickets
+//       </h1>
+//       <p>Tickets are no longer on sale.</p>
+//       <p>Keep an eye out for information about next year...</p>
+//       <TicketPrices />
 
-      <div className="md:flex flex-col items-center min-h-[330px] sm:min-h-[230px]">
-        <div className="md:flex flex-row sm:w-auto sm:p-0 sm:m-0 bg-utd-navy">
-          <DayTickets />
-          <CampingTickets />
-        </div>
-      </div>
+//       <div className="md:flex flex-col items-center min-h-[330px] sm:min-h-[230px]">
+//         <div className="md:flex flex-row sm:w-auto sm:p-0 sm:m-0 bg-utd-navy">
+//           <DayTickets />
+//           <CampingTickets />
+//         </div>
+//       </div>
 
-      <TicketLower />
-    </div>
-  );
-}
+//       <TicketLower />
+//     </div>
+//   );
+// }
